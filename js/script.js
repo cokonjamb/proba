8e46bfc4e58b11d0c0610b195e114293ecf7d052
cokonjamb <cokonjambic@gmail.com>
2017-11-04 12:35:26 +0100

    prvi fajlovi

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var list1 = $('#footer-ul1');
+var list1Items = $('#footer-ul1 li');
+list1.click(function(){
+	list1.toggleClass('list-show');
+	list1Items.toggleClass('list-items-show');
+});
+
+var list2 = $('#footer-ul2');
+var list2Items = $('#footer-ul2 li');
+list2.click(function(){
+	list2.toggleClass('list-show');
+	list2Items.toggleClass('list-items-show');
+});
+
+var list3 = $('#footer-ul3');
+var list3Items = $('#footer-ul3 li');
+list3.click(function(){
+	list3.toggleClass('list-show');
+	list3Items.toggleClass('list-items-show');
+});
+
+var list4 = $('#footer-ul4');
+var list4Items = $('#footer-ul4 li');
+list4.click(function(){
+	list4.toggleClass('list-show');
+	list4Items.toggleClass('list-items-show');
+});
